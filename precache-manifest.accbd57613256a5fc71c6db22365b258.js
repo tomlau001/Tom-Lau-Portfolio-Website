0e45ad25c582315ad71811c43735428d5e0a4c1d
Tom <a@adeiMac.local>
2022-12-02 12:46:13 +0800

    Updates

---
+++ precache-manifest.accbd57613256a5fc71c6db22365b258.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5254df356b3c0f9b1c9",
+    "url": "/Tom-Lau-Portfolio-Website/static/css/main.3d44d868.chunk.css"
+  },
+  {
+    "revision": "c5254df356b3c0f9b1c9",
+    "url": "/Tom-Lau-Portfolio-Website/static/js/main.c5254df3.chunk.js"
+  },
+  {
+    "revision": "73c4a386b51143b368b7",
+    "url": "/Tom-Lau-Portfolio-Website/static/js/1.73c4a386.chunk.js"
+  },
+  {
+    "revision": "d08e3a12ad57cbe81d48",
+    "url": "/Tom-Lau-Portfolio-Website/static/css/2.b80bda5b.chunk.css"
+  },
+  {
+    "revision": "d08e3a12ad57cbe81d48",
+    "url": "/Tom-Lau-Portfolio-Website/static/js/2.d08e3a12.chunk.js"
+  },
+  {
+    "revision": "c3ffad78bc1af8c02fa0",
+    "url": "/Tom-Lau-Portfolio-Website/static/js/runtime~main.c3ffad78.js"
+  },
+  {
+    "revision": "51b894dbf831dd2988fdd19322d7f5b8",
+    "url": "/Tom-Lau-Portfolio-Website/static/media/aboutme.51b894db.jpg"
+  },
+  {
+    "revision": "e2d3c3c5cc2ef0555d4fd0e3ef4d91a8",
+    "url": "/Tom-Lau-Portfolio-Website/static/media/mainImg.e2d3c3c5.jpg"
+  },
+  {
+    "revision": "048a964bdcf5f02a133aea07798aa9bc",
+    "url": "/Tom-Lau-Portfolio-Website/static/media/skill_img.048a964b.jpg"
+  },
+  {
+    "revision": "12adb4dbce567fba1ddcebeb3c4a0f51",
+    "url": "/Tom-Lau-Portfolio-Website/index.html"
+  }
+];
